0f090906d29709bf93f930eef0986a15a11f01cf
Vithursh <vithursh0512@gmail.com>
2025-07-03 23:18:01 -0400

    Captures part of screen screenshot

--- scripts/agent.py
+++ scripts/agent.py
@@ -14,12 +14,12 @@ def start_agent():
     pass
 
 
-# def minimize_window_capture():
-#     root.iconify()
-#     print("Window minimized")
-#     sleep(2)
-#     print("Starting screen capture...")
-#     capture__parts_of_screen()
+def minimize_window_capture():
+    root.iconify()
+    print("Window minimized")
+    sleep(2)
+    print("Starting screen capture...")
+    capture__parts_of_screen()
 
 
 def minimize_window_full():
@@ -40,23 +40,23 @@ def capture_full_screen():
     root.deiconify()
 
 
-# def capture__parts_of_screen():
-#     # Get actual screen size (physical pixels)
-#     user32 = ctypes.windll.user32
-#     user32.SetProcessDPIAware()
-#     screen_width = user32.GetSystemMetrics(0)
-#     screen_height = user32.GetSystemMetrics(1)
+def capture__parts_of_screen():
+    # Get actual screen size (physical pixels)
+    user32 = ctypes.windll.user32
+    user32.SetProcessDPIAware()
+    screen_width = user32.GetSystemMetrics(0)
+    screen_height = user32.GetSystemMetrics(1)
 
-#     print(f"Capturing screen: {screen_width}x{screen_height}")
-#     img = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
-#     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    print(f"Capturing screen: {screen_width}x{screen_height}")
+    img = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-#     x, y, w, h = cv2.selectROI("Drag to select region", img, showCrosshair=True, fromCenter=False)
-#     cv2.destroyAllWindows()
-#     print(f"User picked region at ({x},{y}) with width={w} px and height={h} px")
-#     img = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
-#     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-#     cv2.imwrite("screenshot.png", img)  # Save the screenshot
+    x, y, w, h = cv2.selectROI("Drag to select region", img, showCrosshair=True, fromCenter=False)
+    cv2.destroyAllWindows()
+    print(f"User picked region at ({x},{y}) with width={w} px and height={h} px")
+    img = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite("screenshot.png", img)  # Save the screenshot
     root.deiconify()
 
 
@@ -71,9 +71,9 @@ def main_gui():
     # label = tk.Label(root, text="Welcome to Tkinter")
     # label.pack(pady=10)
 
-    # capture = tk.Button(root, text="Capture part of screen", command=minimize_window_capture, width=22, height=5, font=("Arial", 10))
+    capture = tk.Button(root, text="Capture part of screen", command=minimize_window_capture, width=22, height=5, font=("Arial", 10))
     full_capture = tk.Button(root, text="Full Capture", command=minimize_window_full, width=22, height=5, font=("Arial", 10))
-    # capture.pack(pady=5)
+    capture.pack(pady=5)
     full_capture.pack(pady=5)
 
     # Start the GUI loop
